a14116e3df56e938918d325064da3a696db93e96
CJW <1192190875@qq.com>
2017-04-24 16:46:45 +0800

    cjw firstpush

---
+++ WUshop/Home/Controller/UserController.class.php
@@ -0,0 +1,22 @@
+<?php 
+namespace Home\Controller;
+use Think\Controller;
+
+class UserController extends Controller{
+	/*
+		用户注册
+	*/
+	public function regiter(){
+		$this->display();
+	}
+
+	/*
+		用户登录
+	*/
+	public function login(){
+		$this->display();
+	}
+}
+
+
+?>
---
+++ WUshop/index.php
@@ -0,0 +1,14 @@
+<?php 
+//设置调试模式
+define("APP_DEBUG",TRUE);
+
+//定义静态资源路径
+define("HOME_CSS_URL","/WUshop/WUshop/Home/Common/css");
+define("HOME_IMAGES_URL","/WUshop/WUshop/Home/Common/images");
+
+//引入入口文件
+require("../ThinkPHP/ThinkPHP.php");
+
+
+
+?>
